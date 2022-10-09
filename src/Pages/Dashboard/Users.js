e64b2ac111abf657b57c5d99fdd7883566daf8c2
rishat <rishatrifa56@gmail.com>
2022-10-10 01:41:00 +0600

    doctor add and admin management

---
+++ src/Pages/Dashboard/Users.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
+import UserRow from './UserRow';
+
+const Users = () => {
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/user', {
+        method: 'GET',
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()));
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+    return (
+        <div>
+            <h2 className="text-2xl text-center">All Users: {users.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-3/4 mx-auto my-10">
+                    <thead>
+                        <tr>
+                            <th>Sl.</th>
+                            <th>Email</th>
+                            <th>Action</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) => <UserRow
+                                key={user._id}
+                                user={user}
+                                sl={index}
+                                refetch={refetch}
+                            ></UserRow>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default Users;
