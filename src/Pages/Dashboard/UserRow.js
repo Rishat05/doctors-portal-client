7eee5ec867dd178970d176d93c41d1611fd2676c
rishat <rishatrifa56@gmail.com>
2022-10-11 12:22:11 +0600

    delete user done

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { toast } from 'react-toastify';
+import Swal from "sweetalert2";
 
 const UserRow = ({ user, refetch, sl }) => {
     const { email, role } = user;
@@ -24,12 +25,39 @@ const UserRow = ({ user, refetch, sl }) => {
 
             })
     }
+
+    const handleDlt = async (email) => {
+        const url1 = `https://doctors-portal-743m.onrender.com/allusers/dlt/${email}`;
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this user!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!",
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                await fetch(url1, {
+                    method: "DELETE",
+                    headers: {
+                        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+                    },
+                })
+                    .then((res) => res.json())
+                    .then((data) => {
+                        Swal.fire("Deleted!", "User has been deleted.", "success");
+                        refetch();
+                    });
+            }
+        });
+    };
     return (
         <tr>
             <th>{sl + 1}</th>
             <td>{email}</td>
             <td className='flex gap-3'>{role !== 'admin' && <button onClick={makeAdmin} className="btn btn-xs">Make Admin</button>}
-                <button className="btn btn-xs">Remove User</button></td>
+                <button className="btn btn-xs" onClick={() => handleDlt(email)}>Remove User</button></td>
         </tr>
     );
 };
